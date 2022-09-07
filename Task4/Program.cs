64f8b62e2a3d60e9336b765b10cc18738a90a043
Anton <altay77@inbox.ru>
2022-09-08 00:27:28 +0700

    Task4 solution

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число любой размерности и проверяет, является ли оно палиндромом.
+Console.Write("Введите число: ");
+int n =Convert.ToInt32(Console.ReadLine());
+if (n<0) n=n*-1;
+int i=0, j=1, m=n, k=n;
+    while (m > 10)
+        {   m=m/10;
+            j=j+1;     }
+int[]array = new int[j];
+    while (n > 10)
+        {   array [i]=n%10;
+            n=n/10;
+            i++;    }
+array[i]=n; j=1;
+int palindrome=0;
+    for (i = 0; i < array.Length; i++)
+        {   palindrome=palindrome+array[array.Length-1-i]*j;
+            j=j*10;     }
+if (palindrome == k) Console.Write("Это число палиндром.");
+else Console.Write("Это число не палиндром.");
